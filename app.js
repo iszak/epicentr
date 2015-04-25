59f3d917a0e661332568aff37abce5bead6b9c34
Iszak Bryan <iszak.bryan@gmail.com>
2015-04-25 15:46:13 +0100

    Added sample JSON file

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+
 io.on('connection', function (socket) {
   var disaster = 'earthquake';
   // socket.broadcast.emit(disaster);
