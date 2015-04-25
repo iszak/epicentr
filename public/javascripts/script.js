525b2fb6a336ba4dc90334480ed1288adb79912c
Dominic Walker <dom@Dominics-MacBook-Pro.local>
2015-04-25 16:57:46 +0100

    Merged script files and deleted old

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -9,17 +9,17 @@ if (window.DeviceMotionEvent) {
         console.log(eventData);
 
         var acceleration = eventData.acceleration;
-        info = xyz.replace("X", acceleration.x);
-        info = info.replace("Y", acceleration.y);
-        info = info.replace("Z", acceleration.z);
+        info = xyz.replace("X", acceleration.x.toFixed(2));
+        info = info.replace("Y", acceleration.y.toFixed(2));
+        info = info.replace("Z", acceleration.z.toFixed(2));
         document.getElementById("alpha").innerHTML = info;
 
         console.log(info);
 
         var rotation = eventData.rotationRate;
-        info = xyz.replace('X', rotation.alpha);
-        info = info.replace('Y', rotation.beta);
-        info = info.replace('Z', rotation.gamma);
+        info = xyz.replace('X', rotation.alpha.toFixed(2));
+        info = info.replace('Y', rotation.beta.toFixed(2));
+        info = info.replace('Z', rotation.gamma.toFixed(2));
         document.getElementById("beta").innerHTML = info;
 
         console.log(info);
