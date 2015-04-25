f931dbcebbf71141579b0c249e6b2e6d944caf08
matine <matine.chabrier@gmail.com>
2015-04-25 23:54:40 +0100

    markers

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -89,18 +89,53 @@ var initMap = function(data) {
         mapOptions);
 
     for( i = 0; i < data.data.length; i++ ) {
-        console.log(data.data[i].location.latitude);
+        // console.log(data.data[i].location.latitude);
+        console.log(data);
 
         var position = new google.maps.LatLng(data.data[i].location.latitude, data.data[i].location.longitude);
         bounds.extend(position);
+
+        // var SeverityColors = [
+        //     colorOne: "red",
+        //     colorTwo: "orange",
+        //     colorThree: "yellow",
+        //     colorFour: "white"
+        // ]
+
+        // var min = 20;
+        // var max = 40;
+
+        // var range = max - min; // 20
+        // var section = range / 4; // 5
+
+        // var severityOne = min + section; // 25
+        // var severityTwo = min + ( section * 2 ); // 30
+        // var severityThree = min + ( section * 3 ); // 35
+        // var severityFour = min + ( section * 4 ); // 40
+
+        // var markerColor;
+
+        // if ( )
+
+        // switch ( data.stats ) {
+        //     case min > :
+        //         markerColor = SeverityColors.colorOne;
+        //         break;
+        //     case 0:
+        //         markerColor = SeverityColors.colorOne;
+        //         break;
+        // }
+
+
 
         marker = new google.maps.Marker({
             position: position,
             map: map,
             title: 'marker' + data.data[i]
+            // icon: 'http://maps.google.com/mapfiles/ms/icons/' + ??????????????? + '.png'
         });
-        
-        // // Allow each marker to have an info window    
+
+        // // Allow each marker to have an info window
         // google.maps.event.addListener(marker, 'click', (function(marker, i) {
         //     return function() {
         //         infoWindow.setContent(infoWindowContent[i][0]);
@@ -145,7 +180,7 @@ socket.on('disaster', function (data) {
     if (locationName !== data.location.name) {
         locationName = data.location.name;
 
-        initMap(data);    
+        initMap(data);
     }
 });
 
